019300cc1c60fe7fbf2d679d4c756ee29d2f0725
Abdi <abdirizakomar254@gmail.com>
2022-11-28 07:18:38 +0300

    Added project starter files

---
+++ header.php
@@ -0,0 +1,26 @@
+
+<link rel="stylesheet" href="./Assets/bootstrap/css/bootstrap.min.css" >
+<link rel="stylesheet" href="./Assets/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="./Assets/bootstrap/css/bootstrap.css">
+<link rel="stylesheet" href="./Assets/fontawesome/css/fontawesome.min.css">
+<link rel="stylesheet" href="./Assets/select2/css/select2.min.css"  />
+<link rel="stylesheet" href="./Assets/DataTables/datatables.css"/>
+<link rel="stylesheet" href="./Assets/DataTables/DataTables/css/dataTables.bootstrap5.min.css"/>
+<link rel="stylesheet" href="./Assets/DataTables/twitter-bootstrap.css"/>
+
+
+<script src="./Assets/bootstrap/js/bootstrap.js"></script>
+<script src="./Assets/bootstrap/js/bootstrap.bundle.min.js" ></script>
+<script src="./Assets/jquery.js"></script>
+<script src="./Assets/DataTables/DataTables/js/jquery.dataTables.min.js"></script>
+<script src="./Assets/DataTables/DataTables/js/dataTables.bootstrap5.min.js"></script>
+
+<script src="./Assets/waypoints/jquery.waypoints.min.js"></script>
+
+<script src="./Assets/fontawesome/js/fontawesome.min.js"></script>
+<script src="./Assets/fontawesome/js/all.min.js"></script>
+<script src="./Assets/select2/js/select2.min.js"></script>
+
+
+
+
