6e4a4dac5b26838df6b955d8066edf703cfd697a
Abdi <abdirizakomar254@gmail.com>
2022-11-30 09:14:25 +0300

    perfected database

--- blog.sql
+++ blog.sql
@@ -1,32 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Nov 30, 2022 at 06:44 AM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `blog`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `comments`
---
-
 CREATE TABLE `comments` (
   `commentID` int(11) NOT NULL,
   `commentContent` text NOT NULL,
@@ -39,11 +10,7 @@ CREATE TABLE `comments` (
   `dateUpdated` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `favorites`
---
 
 CREATE TABLE `favorites` (
   `favoritesID` int(11) NOT NULL,
@@ -52,12 +19,7 @@ CREATE TABLE `favorites` (
   `datePosted` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `posts`
---
-DROP TABLE posts;
 CREATE TABLE `posts` (
   `postID` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
@@ -68,15 +30,9 @@ CREATE TABLE `posts` (
   `status` varchar(100) NOT NULL,
   `userID` int(11) NOT NULL,
   `dateCreated` timestamp NOT NULL DEFAULT current_timestamp(),
-  `dateUpdated` timestamp NOT NULL DEFAULT current_timestamp(),
-  
+  `dateUpdated` timestamp NOT NULL DEFAULT current_timestamp()
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `userID` int(11) NOT NULL COMMENT 'tables primary key',
@@ -90,68 +46,54 @@ CREATE TABLE `users` (
   `dateUpdated` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Users Table created abdi helped by nobody';
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `comments`
---
 ALTER TABLE `comments`
-  ADD PRIMARY KEY (`commentID`),
-  ADD KEY `fk_postID` (`postID`),
-  ADD KEY `fk_userID` (`userID`);
+  ADD PRIMARY KEY (`commentID`);
 
---
--- Indexes for table `favorites`
---
+
+
 ALTER TABLE `favorites`
-  ADD PRIMARY KEY (`favoritesID`),
-  ADD KEY `fk_users` (`userID`),
-  ADD KEY `fk_posts` (`postID`);
+  ADD PRIMARY KEY (`favoritesID`);
 
---
--- Indexes for table `posts`
---
+
 ALTER TABLE `posts`
-  ADD PRIMARY KEY (`postID`),
-  ADD KEY `fk_users` (`userID`);
+  ADD PRIMARY KEY (`postID`);
 
---
--- Indexes for table `users`
---
+
 ALTER TABLE `users`
   ADD PRIMARY KEY (`userID`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `comments`
---
 ALTER TABLE `comments`
   MODIFY `commentID` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `favorites`
---
+
 ALTER TABLE `favorites`
   MODIFY `favoritesID` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `posts`
---
+
 ALTER TABLE `posts`
   MODIFY `postID` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `users`
---
+
 ALTER TABLE `users`
   MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'tables primary key';
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+ALTER TABLE `posts`
+  ADD CONSTRAINT `fk_user_pos` FOREIGN KEY (`userid`) REFERENCES `users` (`userID`);
+
+
+ALTER TABLE `favorites`
+  ADD CONSTRAINT `fk_user_fav` FOREIGN KEY (`userid`) REFERENCES `users` (`userID`);
+
+ALTER TABLE `favorites`
+  ADD CONSTRAINT `fk_posts_fav` FOREIGN KEY (`postID`) REFERENCES `posts` (`postID`);
+
+ALTER TABLE `comments`
+  ADD CONSTRAINT `fk_user_com` FOREIGN KEY (`userid`) REFERENCES `users` (`userID`);
+
+ALTER TABLE `comments`
+  ADD CONSTRAINT `fk_posts_com` FOREIGN KEY (`postID`) REFERENCES `posts` (`postID`);
+COMMIT;
