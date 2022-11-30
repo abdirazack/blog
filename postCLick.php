734ab6943090360bcac0db221a6a0b122c95216d
Kamaal22 <Kamaalkcy@gmail.com>
2022-11-30 22:05:35 +0300

    nothing

--- postCLick.php
+++ postCLick.php
@@ -4,13 +4,13 @@
   <div class="card-header">
     Title
   </div>
-  <div class="card-body bordered">
+  <div class="card-body bordered shadow">
     <div class="container-fluid posts-content">
-        <div class="col-lg-6">
+        <div class="col-lg-6 shadow-lg">
             <div class="card mb-4">
-              <div class="card-body">
+              <div class="card-body ">
                 <div class="media mb-3">
-                  <img src="./Pictures/Profiles/bp.jpg" class="d-block ui-w-40 rounded-5"  style="width: 24rem;" alt="">
+                  <img src="./Pictures/Profiles/bp.jpg" class="d-block ui-w-40 rounded"  style="width: 100%;" alt="">
                   <div class="media-body ml-3">
                    User: Kenneth Frazier
                     <div class="text-muted small">DateCreated: 3 days ago</div>
@@ -27,7 +27,7 @@
                 <a href="javascript:void(0)" class="d-inline-block text-muted">
                 
                   <small class="align-middle ">
-                    <i class="fab fab-regular fa-heart"></i>Favourites</small>
+                    <i class="fas fa-heart"></i>Favourites</small>
                 </a>
                 <a href="javascript:void(0)" class="d-inline-block text-muted ml-3">
                   <small class="align-middle">
