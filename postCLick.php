dd6f4c2f5758befa700976519fa1e1b004d29adb
Kamaal22 <Kamaalkcy@gmail.com>
2022-11-30 22:45:52 +0300

    postClick modified again

--- postCLick.php
+++ postCLick.php
@@ -1,17 +1,16 @@
 <?php include_once("navabar.php");?>
 
-<div class="card" style="width: 100%;">
-    <div class="card-header">
-        Title
-    </div>
-    <div class="card-body bordered">
-        <div class="container-fluid posts-content">
+
+
+        <div class="card card-lg container-fluid posts-content bordered" style="width: 100%;">
             <div class="col-lg-6">
                 <div class="card mb-4">
-                    <div class="card-body shadow">
+                <div class="card-header" style="{width: 100%;}">
+                  Title
+                  </div>
+                    <div class="card-body shadow "style="width: 100%;">
                         <div class="media mb-3">
-                            <img src="./Pictures/Profiles/bp.jpg" class="d-block ui-w-40 rounded-5" style="width: 100%;"
-                                alt="">
+                            <img src="./Pictures/Profiles/bp.jpg" class="d-block ui-w-40 rounded-5" style="width: 100%;" alt="">
                             <div class="media-body ml-3">
                                 User: Kenneth Frazier
                                 <div class="text-muted small">DateCreated: 3 days ago</div>
@@ -27,24 +26,28 @@
                     </div>
 
                     <div class="card-footer">
-                        <a href="javascript:void(0)" class="d-inline-block text-muted">
-
-                            <small class="align-middle ">
-                                <i class="fab fab-regular fa-heart"></i>Favourites</small>
+                        <a href="javascript:void(0)" class="d-inline-block text-decoration-none">
+                          <i class="fas fa-heart"></i> 
+                            <small class="align-middle p-3">
+                                <!--Favourite-->
+                            </small>
                         </a>
-                        <a href="javascript:void(0)" class="d-inline-block text-muted ml-3">
+                        <a href="javascript:void(0)" class="d-inline-block text-decoration-none ml-3">
+                        <i class="fas fa-comment align-middle text-muted "></i>
+                            <small class=" p-3 align-middle"> 
+                                <!--Comment Count-->
+                              </small>
+                        </a>
+                        <a href="javascript:void(0)" class="d-inline-block text-decoration-none ml-3">
+                            <i class="fas fa-eye align-middle text-muted "></i>
                             <small class="align-middle">
-                                <i icon="fa-solid fa-comment"></i>
-                                Comments</small>
+                              <!--View Count-->
+                            </small>
                         </a>
-                        <a href="javascript:void(0)" class="d-inline-block text-muted ml-3">
-                            <i class="fas fa-heart align-middle"></i>&nbsp;
-                            <small class="align-middle">Repost</small>
-                        </a>
-
+                        <?php
+                        include_once("comments.php");
+                        ?>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
