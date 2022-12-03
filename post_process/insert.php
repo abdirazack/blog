14e1ce9de300457ac72d189aadad06f4fbaa6f0e
Abdi <abdirizakomar254@gmail.com>
2022-12-03 18:18:20 +0300

    nothing

--- post_process/insert.php
+++ post_process/insert.php
@@ -1,5 +1,5 @@
 <?php 
-    require_once '../db_connect.php';
+    require_once '../db_connect.php'; 
 
     if(isset($_POST['btnReg']) && isset($_FILES['avatar'])){
 
--- viewPost.php
+++ viewPost.php
@@ -30,16 +30,16 @@
 
 
 <div class="container">
-        <div class="card mb-4">
+    <div class="card mb-4">
 
-            <div class="card-header">
-                <h2><?php echo $theTitle;?></h2>
-            </div>
+        <div class="card-header">
+            <h2><?php echo $theTitle;?></h2>
+        </div>
 
-            <div class="card-body ">
+        <div class="card-body ">
 
 
-                <?php //Username
+            <?php //Username
                             echo " <strong>$userName</strong>";
                            //Date Created
                                  $dateCreated = strtotime($dateCreated); echo "<br>";
@@ -47,39 +47,39 @@
                                  echo "<small>". time_elapsed_string($dateCreated) ."</small> ";
                         ?>
 
-                <hr>
-                <p>
-                    <!-- //Post Image -->
-                    <img src="<?php echo $imgPath; ?>" class="d-block ui-w-40 rounded-3 w-100" alt="">
-                    <!-- //Post Content -->
-                    <?php  echo $postDesc;?>
-                </p>
+            <hr>
+            <p>
+                <!-- //Post Image -->
+                <img src="<?php echo $imgPath; ?>" class="d-block ui-w-40 rounded-3 w-100" alt="">
+                <!-- //Post Content -->
+                <?php  echo $postDesc;?>
+            </p>
 
-            </div>
+        </div>
 
-            <div class="card-footer">
-                <a href="javascript:void(0)" class="d-inline-block text-decoration-none">
-                    <i class="fas fa-heart"></i>
-                    <small class="align-middle p-3">
-                        <?php echo $commentCount;?>
-                    </small>
-                </a>
-                <a href="javascript:void(0)" class="d-inline-block text-decoration-none ml-3">
-                    <i class="fas fa-comment align-middle text-muted "></i>
-                    <small class=" p-3 align-middle">
-                        <?php echo $commentCount;?>
-                    </small>
-                </a>
-                <a href="javascript:void(0)" class="d-inline-block text-decoration-none ml-3">
-                    <i class="fas fa-eye align-middle text-muted "></i>
-                    <small class="align-middle">
-                        <!--View Count-->
-                        <?php echo $views;?>
-                    </small>
-                </a>
-                <?php
+        <div class="card-footer">
+            <a href="javascript:void(0)" class="d-inline-block text-decoration-none">
+                <i class="fas fa-heart"></i>
+                <small class="align-middle p-3">
+                    <?php echo $commentCount;?>
+                </small>
+            </a>
+            <a href="javascript:void(0)" class="d-inline-block text-decoration-none ml-3">
+                <i class="fas fa-comment align-middle text-muted "></i>
+                <small class=" p-3 align-middle">
+                    <?php echo $commentCount;?>
+                </small>
+            </a>
+            <a href="javascript:void(0)" class="d-inline-block text-decoration-none ml-3">
+                <i class="fas fa-eye align-middle text-muted "></i>
+                <small class="align-middle">
+                    <!--View Count-->
+                    <?php echo $views;?>
+                </small>
+            </a>
+            <?php
                         include_once("comments.php");
                 ?>
-            </div>
         </div>
-        </div>
+    </div>
+</div>
