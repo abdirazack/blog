6b2a700d7428716ca55d0902ddd5fad71088adc9
Abdi <abdirizakomar254@gmail.com>
2022-12-13 08:43:54 +0300

    fixed landing page

---
+++ index_process.php
@@ -0,0 +1,60 @@
+<?php
+    require_once("./db_connect.php");
+    $postID = "";
+    if(isset($_POST['displaysend'])){
+
+        $query=mysqli_query($conn,"select * from posts  order by viewCount desc limit 3");
+        if(mysqli_num_rows($query)>0){
+        while($rows=mysqli_fetch_array($query)){
+            $postID = $rows['postID'];
+        ?>
+
+
+
+<div class="col">
+<div class="card m-1 p-0" style="width: 100%;">
+
+    <div class="card-header  bg-light ml-0 p-0 " style="font-family: 'poppins';">
+        <h5 class="m-2 text-uppercase text-center"> <?php echo $rows['title']  ?></h5>
+    </div>
+
+    <div class="card-body m-0 p-0">
+        <!-- <p class="card-text">  <?php echo $rows['content']  ?> </p> -->
+        <div class="card-img">
+            <img src="<?php echo $rows['picture']  ?>" class="mt-1 mb-1 p-0" style="width: 100%; height: 250px;" alt="Post picture">
+        </div>
+    </div>
+
+    <div class="card-footer bg-primary m-0 p-0" style="height: 40px;">
+    <style>
+        .btn:hover{
+            color: white;
+            background-color: darkblue;
+        }
+    </style>
+        <Button onclick="viewPost()" class="btn btn-succes h-100 m-0 p-0 w-100 " style="font-family: 'poppins'; font-weight:600;">View Post</Button>
+    </div>
+
+</div>
+</div>
+<?php
+        } 
+    }
+     else{
+        $data = ['message'=>'No Data found in posts', 'status'=>404];
+        echo json_encode($data);
+        return ;
+    }
+}
+    else{
+        $data = ['message'=>'Button not set', 'status'=>404];
+        echo json_encode($data);
+        return ;
+    }
+
+?>
+<script>
+function viewPost() {
+    window.location.assign('./userReg.php');
+}
+</script>
--- navabar.php
+++ navabar.php
@@ -15,52 +15,74 @@
   }
 
 ?>
+
 <head>
-<link rel="icon" type="image/x-icon" href="./Pictures/icon.ico">
-<title>COWt</title>
+    <link rel="icon" type="image/x-icon" href="./Pictures/icon.ico">
+    <title>COWt</title>
 </head>
 
-<nav class="navbar navbar-expand-lg bg-light shadow p-3 mb-5 bg-body rounded">
-<div class="container">
-    <a class="navbar-brand" href="#">COWT</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse text-secondary" id="navbarNavDropdown" >
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item px-3">
-          <a class="nav-link active " aria-current="page" href="./home.php">Home</a>
-        </li>
-        <li class="nav-item px-3">
-          <a class="nav-link" href="./viewPost.php">Features</a>
-        </li>
-        <li class="nav-item px-3">
-          <a class="nav-link" href="home.php">Create Post</a>
-        </li>
-        <li class="nav-item dropdown px-3">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          <img src="<?php echo $userProfilePicture; ?>" class="rounded-5 mx-auto d-block" alt="Profile picure" height="50px" width="50px">
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="./profile.php">Profile</a></li>
-            <li><a class="dropdown-item" href="#">Settings</a></li>
-            <button type="button" class="btn btn-toggle" data-toggle="button" aria-pressed="false" autocomplete="off">
-        <div class="handle"></div>
-      </button>
-            <li><a class="dropdown-item" href="./logout.php">Log Out</a></li>
-          </ul>
-        </li>
-      </ul>
+<nav class="navbar navbar-expand-lg bg-light shadow p-3 mb-5 bg-body rounded" id="navbar">
+    <div class="container">
+        <a class="navbar-brand" href="#">COWT</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse text-secondary" id="navbarNavDropdown">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item px-3">
+                    <a class="nav-link active " aria-current="page" href="./home.php">Home</a>
+                </li>
+                <li class="nav-item px-3">
+                    <a class="nav-link" href="./viewPost.php">Features</a>
+                </li>
+                <li class="nav-item px-3">
+                    <a class="nav-link">
+                        <div class="form-check form-switch" id="darkmode">
+                            <span> dark mode</span>
+                            <input type="checkbox" id="darkmode" class="form-check-input">
+                        </div>
+                    </a>
+                </li>
+                <li class="nav-item dropdown px-3">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        <img src="<?php echo $userProfilePicture; ?>" class="rounded-5 mx-auto d-block"
+                            alt="Profile picure" height="50px" width="50px">
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="./profile.php">Profile</a></li>
+                        <li><a class="dropdown-item" href="./logout.php">Log Out</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>
-  </div>
 </nav>
 
 <script>
-$(document).ready(function(){
-  $(".nav-item").hover(function(){
-    $(this).css("border-bottom", "2px solid #10a9e0");
-    }, function(){
-    $(this).css("border", "0");
-  });
+$(document).ready(function() {
+    $(".nav-item").hover(function() {
+        $(this).css("border-bottom", "2px solid #10a9e0");
+    }, function() {
+        $(this).css("border", "0");
+    });
+
+
+    $("#darkmode").click(function() {
+
+        $("#createPost").removeClass("bg-light");
+        $("#createPost").addClass("bg-dark");
+        $("#createPost").addClass("text-black");
+
+        $("body").removeClass("bg-light");
+        $("body").addClass("bg-dark");
+        $("body").addClass("text-white");
+
+        $("#navbar").removeClass("bg-light");
+        $("#navbar").addClass("bg-dark");
+        $("#navbar").addClass("text-white");
+
+    });
 });
 </script>
